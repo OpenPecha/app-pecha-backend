4bec04c2993a13a1f35c4ea79f0a5c59e1634ae4
tenzinsamten <iamsamten>
2024-12-25 10:55:46 +0100

    chore: get float key and database url from config

--- pecha_api/config.py
+++ pecha_api/config.py
@@ -12,6 +12,7 @@ DEFAULTS = dict(
     REFRESH_TOKEN_EXPIRE_DAYS=30,
     SENDGRID_API_KEY="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",
     SENDGRID_SENDER_EMAIL="samten@esukhia.org",
+    DATABASE_URL="postgresql://admin:pechaAdmin@localhost:5434/pecha",
     VERSION="0.0.1"
 
 )
@@ -25,4 +26,9 @@ def get(key: str) -> str:
 
 
 def get_float(key: str) -> float:
-    return float(get(key))
+    try:
+        return float(get(key))
+    except (TypeError, ValueError) as e:
+        raise ValueError(f"Could not convert the value for key '{key}' to float: {e}")
+
+
