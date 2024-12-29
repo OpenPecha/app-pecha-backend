e274a2d45ea573f009762e26330df93f7b8156fb
tenzinsamten <iamsamten>
2024-12-29 12:34:42 +0100

    chore: update alembic env file

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-
+from pecha_api.config import get
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -10,6 +10,10 @@ from pecha_api.users.users_models import Users
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+database_url = get("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
