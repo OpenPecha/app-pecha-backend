d3ac970d07345c25eb7ba7c9b04ee8ec4b714dd6
tenzinsamten <iamsamten>
2025-02-02 13:46:14 +0100

    chore: update the sheets api

--- pecha_api/sheets/sheets_views.py
+++ pecha_api/sheets/sheets_views.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from fastapi.security import HTTPBearer
 from starlette import status
 
+from .sheets_service import get_sheets
+
 oauth2_scheme = HTTPBearer()
 sheets_router = APIRouter(
     prefix="/sheets",
@@ -12,4 +14,4 @@ sheets_router = APIRouter(
 
 @sheets_router.get("", status_code=status.HTTP_200_OK)
 def get_topics(language: str | None):
-    return ""
+    return get_sheets(topic_id='', language=language)
