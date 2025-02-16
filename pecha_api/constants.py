69eb61b2af131bb62f8d98852aeb18e245f1d54f
tenzinsamten <iamsamten>
2025-02-16 20:39:25 +0100

    tst: update the parent id invalid

--- pecha_api/constants.py
+++ pecha_api/constants.py
@@ -1,6 +1,10 @@
+import logging
 from typing import Optional
 
 from beanie import PydanticObjectId
+from bson.errors import InvalidId
+from fastapi import HTTPException
+from starlette import status
 
 
 def get_value_from_dict(values: dict[str, str], language: str):
@@ -10,5 +14,10 @@ def get_value_from_dict(values: dict[str, str], language: str):
 def get_parent_id(parent_id: Optional[str]):
     topic_parent_id = None
     if parent_id is not None:
-        topic_parent_id = PydanticObjectId(parent_id)
+        try:
+            topic_parent_id = PydanticObjectId(parent_id)
+        except InvalidId as e:
+            logging.debug("error with id: {}", e)
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Parent id")
+
     return topic_parent_id
