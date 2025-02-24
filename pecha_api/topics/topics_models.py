84f31f83f6939e5a8bff0d18e103d1ad1c202ae3
Tenzin Tsering <tenzintsering630@gmail.com>
2025-02-24 18:53:10 +0530

    changes to error in the else part of search query

--- pecha_api/topics/topics_models.py
+++ pecha_api/topics/topics_models.py
@@ -35,7 +35,6 @@ class Topic(Document):
                 query["parent_id"] = None
                 query[f"titles.{language}"] = {"$regex": f"^{search}", "$options": "i"}
             else:
-                query["parent_id"] = {"$ne": None}
                 query[f"titles.{language}"] = {"$regex": f"^{search}", "$options": "i"}
         elif parent_id:
             query["parent_id"] = parent_id
