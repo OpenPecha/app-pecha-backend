a02b9991354fbdcd5f974c7bd806f3927fd8ecc7
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-03-06 15:50:36 +0530

    Redefine MongoDB sheet model

--- pecha_api/sheets/sheets_models.py
+++ pecha_api/sheets/sheets_models.py
@@ -1,25 +1,25 @@
 import uuid
-from typing import Dict, List
+from typing import Dict, List, Union
 
+from beanie import PydanticObjectId
 from pydantic import BaseModel,Field
 
 
 class Sheet(BaseModel):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    # id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
+    publisher_id: str
     titles: Dict[str, str] = Field(default_factory={})
     summaries: Dict[str, str] = Field(default_factory={})
-    date: str
-    views: str
-    topics: List[Dict[str, str]] = Field(default_factory=[])
+    source: List[Dict[str, Union[Dict, str]]] = Field(default_factory=[])
+    creation_date: str
+    modified_date: str
+    published_date: str
     published_time: int
-    publisher_id: str
-    #topic_id
-    #created date
-    #modified date
-    #published date
-    #views
-    #likes
+    views: str
+    likes: List[Dict[str, str]] = Field(default_factory=[])
+    collection: List[str] = Field(default_factory=[])
+    topics: List[Dict[str, str]] = Field(default_factory=[]),
 
-    
 
 
