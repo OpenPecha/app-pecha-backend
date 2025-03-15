2fa103c20f1cbe875fbfdff1b0d23f20f3cee01e
tenzinsamten <iamsamten>
2025-03-15 11:55:21 +0100

    chore: directory for mappings and section

--- pecha_api/texts/texts_views.py
+++ pecha_api/texts/texts_views.py
@@ -5,10 +5,9 @@ from starlette import status
 
 from typing import Optional
 
-from .texts_service import get_contents_by_text_id, get_versions_by_text_id, create_new_text, create_new_segment, get_text_by_term_or_category
+from .texts_service import get_contents_by_text_id, get_versions_by_text_id, create_new_text, create_new_segment, \
+    get_text_by_term_or_category
 from .texts_response_models import CreateTextRequest, CreateSegmentRequest
-
-
 
 oauth2_scheme = HTTPBearer()
 text_router = APIRouter(
@@ -19,37 +18,37 @@ text_router = APIRouter(
 
 @text_router.get("", status_code=status.HTTP_200_OK)
 async def get_text(
-    category: Optional[str]= Query(default=None),
-    language: str = Query(default=None),
-    skip: int= Query(default=0),
-    limit: int= Query(default=10)
+        category: Optional[str] = Query(default=None),
+        language: str = Query(default=None),
+        skip: int = Query(default=0),
+        limit: int = Query(default=10)
 ):
     return await get_text_by_term_or_category(category=category, language=language, skip=skip, limit=limit)
 
-
-@text_router.get("/{text_id}/contents", status_code=status.HTTP_200_OK)
-async def get_contents(
-    text_id: str,
-    skip: int = Query(default=0),
-    limit: int = Query(default=10)
-):
-    return await get_contents_by_text_id(text_id=text_id, skip=skip, limit=limit)
-
-@text_router.get("/{text_id}/versions", status_code=status.HTTP_200_OK)
-async def get_versions(
-    text_id: str,
-    skip: int = Query(default=0),
-    limit: int= Query(default=10)
-):
-    return await get_versions_by_text_id(text_id=text_id, skip=skip, limit=limit)
 
 @text_router.post("", status_code=status.HTTP_201_CREATED)
 async def create_text(create_text_request: CreateTextRequest):
     return await create_new_text(create_text_request=create_text_request)
 
-@text_router.post("/segment", status_code=status.HTTP_201_CREATED)
+
+@text_router.get("/{text_id}/contents", status_code=status.HTTP_200_OK)
+async def get_contents(
+        text_id: str,
+        skip: int = Query(default=0),
+        limit: int = Query(default=10)
+):
+    return await get_contents_by_text_id(text_id=text_id, skip=skip, limit=limit)
+
+
+@text_router.get("/{text_id}/versions", status_code=status.HTTP_200_OK)
+async def get_versions(
+        text_id: str,
+        skip: int = Query(default=0),
+        limit: int = Query(default=10)
+):
+    return await get_versions_by_text_id(text_id=text_id, skip=skip, limit=limit)
+
+
+@text_router.post("/segments", status_code=status.HTTP_201_CREATED)
 async def create_segment(create_segment_request: CreateSegmentRequest):
     return await create_new_segment(create_segment_request=create_segment_request)
-
-
-
