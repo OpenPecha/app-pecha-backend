38cd1bc95cf7fcb5bc0dc782814e5125a155110b
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-03-26 15:10:33 +0530

    test case for get versions by text id in texts_service

--- tests/texts/test_texts_service.py
+++ tests/texts/test_texts_service.py
@@ -3,8 +3,8 @@ import uuid
 
 from pecha_api.terms.terms_response_models import TermsModel
 import pytest
-from pecha_api.texts.texts_service import create_new_text
-from pecha_api.texts.texts_response_models import CreateTextRequest, TextModel, Text
+from pecha_api.texts.texts_service import create_new_text, get_versions_by_text_id
+from pecha_api.texts.texts_response_models import CreateTextRequest, TextModel, Text, TextVersion, TextVersionResponse
 from pecha_api.texts.texts_service import get_text_by_term_or_category, TextsCategoryResponse
 
 @pytest.mark.asyncio
@@ -76,6 +76,86 @@ async def test_get_text_by_category():
             total=len(mock_texts_by_category),
             skip=0,
             limit=10
+        )
+
+@pytest.mark.asyncio
+async def test_get_versions_by_text_id():
+    text_detail = TextModel(
+        id="id_1",
+        title="བྱང་ཆུབ་སེམས་དཔའི་སྤྱོད་པ་ལ་འཇུག་པ།",
+        language="bo",
+        type="root_text",
+        is_published=True,
+        created_date="2025-03-20 09:26:16.571522",
+        updated_date="2025-03-20 09:26:16.571532",
+        published_date="2025-03-20 09:26:16.571536",
+        published_by="pecha",
+        categories=["67dd22a8d9f06ab28feedc90"],
+        parent_id=None
+    )
+    versions_by_text_id = [
+        TextVersion(
+            id="59769286-2787-4181-953d-9149cdeef959",
+            title= "The Way of the Bodhisattva",
+            parent_id= "032b9a5f-0712-40d8-b7ec-73c8c94f1c15",
+            priority= None,
+            language= "en",
+            type= "version",
+            is_published= True,
+            created_date= "2025-03-20 09:28:28.076920",
+            updated_date= "2025-03-20 09:28:28.076934",
+            published_date= "2025-03-20 09:28:28.076938",
+            published_by= "pecha"
+        ),
+        TextVersion(
+            id= "ee88cb8b-42b2-45af-a6a4-753c0e9d779c",
+            title= "शबोधिचर्यावतार",
+            parent_id= "032b9a5f-0712-40d8-b7ec-73c8c94f1c15",
+            priority= None,
+            language= "sa",
+            type= "version",
+            is_published= True,
+            created_date= "2025-03-20 09:29:51.154697",
+            updated_date= "2025-03-20 09:29:51.154708",
+            published_date= "2025-03-20 09:29:51.154712",
+            published_by= "pecha"
+        )
+    ]
+    with patch('pecha_api.texts.texts_service.get_texts_by_id', new_callable=AsyncMock) as mock_text_detail, \
+        patch('pecha_api.texts.texts_service.get_versions_by_id', new_callable=AsyncMock) as mock_get_versions_by_text_id:
+        mock_text_detail.return_value = text_detail
+        mock_get_versions_by_text_id.return_value = versions_by_text_id
+        response = await get_versions_by_text_id(text_id="id_1", skip=0, limit=10)
+        assert response == TextVersionResponse(
+            text=TextModel(
+                id=text_detail.id,
+                title=text_detail.title,
+                language=text_detail.language,
+                type=text_detail.type,
+                is_published=text_detail.is_published,
+                created_date=text_detail.created_date,
+                updated_date=text_detail.updated_date,
+                published_date=text_detail.published_date,
+                published_by=text_detail.published_by,
+                categories=text_detail.categories,
+                parent_id=text_detail.parent_id
+            ),
+            versions=[
+                TextVersion(
+                    id=version.id,
+                    title=version.title,
+                    parent_id=version.parent_id,
+                    priority=version.priority,
+                    language=version.language,
+                    type=version.type,
+                    is_published=version.is_published,
+                    created_date=version.created_date,
+                    updated_date=version.updated_date,
+                    published_date=version.published_date,
+                    published_by=version.published_by
+                )
+                for version in versions_by_text_id
+            ]
         )
 
 
