952f2d17fd6714f2642bcf103cc40b6dacbc7999
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-05-03 14:33:23 +0530

    added group document in db setting

--- pecha_api/db/mongo_database.py
+++ pecha_api/db/mongo_database.py
@@ -11,6 +11,7 @@ from ..texts.texts_models import Text
 from ..texts.segments.segments_models import Segment
 from ..sheets.sheets_models import Sheet
 from ..texts.texts_models import TableOfContent
+from ..texts.groups.groups_models import Group
 from ..config import get
 
 mongodb_client = None
@@ -27,7 +28,7 @@ async def lifespan(api: FastAPI):
 
     # Initialize collections and indexes if necessary
     try:
-        await init_beanie(database=mongodb,document_models=[Term, Topic, Text, Segment, Sheet, TableOfContent])
+        await init_beanie(database=mongodb,document_models=[Term, Topic, Text, Segment, Sheet, TableOfContent, Group])
         logging.info("Beanie initialized with the 'terms' collection.")
         
     except Exception as e:
