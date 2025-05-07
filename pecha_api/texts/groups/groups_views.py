2f14a1a2384c5e31b7c375c14159fadb8af96b85
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-05-07 10:27:08 +0530

    added get group by group_id GET endpoint

--- pecha_api/texts/groups/groups_views.py
+++ pecha_api/texts/groups/groups_views.py
@@ -5,9 +5,11 @@ from starlette import status
 
 from .groups_response_models import (
     CreateGroupRequest,
+    GroupDTO
 )
 from .groups_service import (
-    create_new_group
+    create_new_group,
+    get_group_details
 )
 
 oauth2_scheme = HTTPBearer()
@@ -16,11 +18,19 @@ group_router = APIRouter(
     tags=["Groups"]
 )
 
+@group_router.get("/{group_id}", status_code=status.HTTP_200_OK)
+async def get_group_by_id(
+    group_id: str
+) -> GroupDTO:
+    return await get_group_details(
+        group_id=group_id
+    )
+
 @group_router.post("", status_code=status.HTTP_201_CREATED)
 async def create_group(
     create_group_request: CreateGroupRequest,
     authentication_credential: Annotated[HTTPAuthorizationCredentials, Depends(oauth2_scheme)],
-):
+) -> GroupDTO:
     return await create_new_group(
         create_group_request=create_group_request,
         token=authentication_credential.credentials
