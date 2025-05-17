e9395c0b3fe3fbff52833c944d68e1cd276eecf1
tenzinsamten <iamsamten>
2025-05-17 10:45:50 +0200

    chore: Code refactor

--- pecha_api/texts/texts_service.py
+++ pecha_api/texts/texts_service.py
@@ -1,28 +1,26 @@
-
-
 from fastapi import HTTPException
 from starlette import status
 
 from pecha_api.error_contants import ErrorConstants
 from .texts_repository import (
-    get_texts_by_term, 
-    get_texts_by_group_id, 
+    get_texts_by_term,
+    get_texts_by_group_id,
     create_text,
     create_table_of_content_detail,
-    get_contents_by_id, 
+    get_contents_by_id,
     get_table_of_content_by_content_id,
     get_sections_count_of_table_of_content
 )
 from .texts_response_models import (
-    TableOfContent, 
-    DetailTableOfContentResponse, 
-    TableOfContentResponse, 
-    TextModel, 
-    TextVersionResponse, 
+    TableOfContent,
+    DetailTableOfContentResponse,
+    TableOfContentResponse,
+    TextModel,
+    TextVersionResponse,
     TextVersion,
-    TextsCategoryResponse, 
-    Text, 
-    CreateTextRequest, 
+    TextsCategoryResponse,
+    Text,
+    CreateTextRequest,
     TextDetailsRequest,
     DetailTextMapping
 )
@@ -43,44 +41,6 @@ from typing import List
 from pecha_api.config import get
 
 
-async def get_texts_by_term_id(term_id: str, language: str, skip: int, limit: int) -> List[Text]:
-    texts = await get_texts_by_term(term_id=term_id, language=language, skip=skip, limit=limit)
-    filter_text_base_on_group_id_type = await TextUtils.filter_text_base_on_group_id_type(texts=texts, language=language)
-    root_text = filter_text_base_on_group_id_type["root_text"]
-    commentary = filter_text_base_on_group_id_type["commentary"]
-    text_list = [
-        Text(
-            id=str(text.id),
-            title=text.title,
-            language=text.language,
-            type="commentary",
-            group_id=text.group_id,
-            is_published=text.is_published,
-            created_date=text.created_date,
-            updated_date=text.updated_date,
-            published_date=text.published_date,
-            published_by=text.published_by
-        )
-        for text in commentary
-    ]
-    if root_text is not None:
-        text_list.append(
-            Text(
-                id=str(root_text.id),
-                title=root_text.title,
-                language=root_text.language,
-                type="root_text",
-                group_id=root_text.group_id,
-                is_published=root_text.is_published,
-                created_date=root_text.created_date,
-                updated_date=root_text.updated_date,
-                published_date=root_text.published_date,
-                published_by=root_text.published_by
-            )
-        )
-    return text_list
-
-
 async def get_text_by_text_id_or_term(
         text_id: str,
         term_id: str,
@@ -93,7 +53,7 @@ async def get_text_by_text_id_or_term(
 
     if term_id is not None:
         term = await get_term(term_id=term_id, language=language)
-        texts = await get_texts_by_term_id(term_id=term_id, language=language,skip=skip, limit=limit)
+        texts = await _get_texts_by_term_id(term_id=term_id, language=language, skip=skip, limit=limit)
         return TextsCategoryResponse(
             term=term,
             texts=texts,
@@ -105,16 +65,17 @@ async def get_text_by_text_id_or_term(
         return await TextUtils.get_text_detail_by_id(text_id=text_id)
 
 
-async def get_table_of_contents_by_text_id(text_id: str, skip: int, limit: int) -> TableOfContentResponse:
-    is_valid_text = await TextUtils.validate_text_exists(text_id=text_id)
+async def get_table_of_contents_by_text_id(text_id: str) -> TableOfContentResponse:
+    is_valid_text: bool = await TextUtils.validate_text_exists(text_id=text_id)
     if not is_valid_text:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ErrorConstants.TEXT_NOT_FOUND_MESSAGE)
     text = await TextUtils.get_text_detail_by_id(text_id=text_id)
     list_of_table_of_contents = await get_contents_by_id(text_id=text_id)
-    table_of_contents = TextUtils.remove_segments_from_list_of_table_of_content(table_of_content=list_of_table_of_contents)
+    table_of_contents = TextUtils.remove_segments_from_list_of_table_of_content(
+        table_of_content=list_of_table_of_contents)
     return TableOfContentResponse(
         text_detail=text,
-        contents= [
+        contents=[
             TableOfContent(
                 id=str(content.id),
                 text_id=content.text_id,
@@ -124,117 +85,48 @@ async def get_table_of_contents_by_text_id(text_id: str, skip: int, limit: int) 
         ]
     )
 
+
 async def get_text_details_by_text_id(
-    text_id: str,
-    text_details_request: TextDetailsRequest
+        text_id: str,
+        text_details_request: TextDetailsRequest
 ) -> DetailTableOfContentResponse:
-
-    # Check if text_id is provided
-    if text_id is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ErrorConstants.TEXT_OR_TERM_NOT_FOUND_MESSAGE
-        )
-
-
-    # Check if valid version_id is provided
-    if text_details_request.version_id is not None:
-        is_valid_version = await TextUtils.validate_text_exists(
-            text_id=text_details_request.version_id
-        )
-        if not is_valid_version:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=ErrorConstants.VERSION_NOT_FOUND_MESSAGE
-            )
-
-    # Check if valid segment_id is provided
-    if text_details_request.segment_id is not None:
-        is_valid_segment = await SegmentUtils.validate_segment_exists(
-            segment_id=text_details_request.segment_id
-        )
-        if not is_valid_segment:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=ErrorConstants.SEGMENT_NOT_FOUND_MESSAGE
-            )
-
-    is_valid_text = await TextUtils.validate_text_exists(text_id=text_id)
-
-    if is_valid_text:
-        '''
-        In the following process first we will get the table of content if content_id is provided
-        Otherwise the frontend should have send segment_id which means we'll have to search this segment_id in table of contents
-        Later after getting the table of content we need to map each segment_id with it's content
-        If version ID is provied then we need to first identify the text_id in the mapping field and check if it's matching with the version_id, if yes we just map the segment_id with it's content
-        '''
-        text = await TextUtils.get_text_detail_by_id(text_id=text_id)
-        table_of_content = None
-        if text_details_request.content_id is not None:
-            table_of_content = await get_table_of_content_by_content_id(
-                content_id=text_details_request.content_id,
-                skip=text_details_request.skip,
-                limit=text_details_request.limit
-            )
-        elif text_details_request.segment_id is not None:
-            table_of_content = await TextUtils.get_table_of_content_id_and_respective_section_by_segment_id(
-                text_id=text_id,
-                segment_id=text_details_request.segment_id
-            )
-            text_details_request.skip = max(0, table_of_content.sections[0].section_number - 1)
-            text_details_request.limit = 1
-
-        # Check if the table of content exists in the cache database
-        cached_data = await get_text_details_cache(
-            text_id=text_id,
-            content_id=str(table_of_content.id),
-            version_id=text_details_request.version_id,
-            skip=text_details_request.skip,
-            limit=text_details_request.limit
-        )
-        # if the same table of content with same skip and limit is again found when searching segments in toc. It's returned from cache
-        if cached_data is not None:
-            return cached_data
-
-        if table_of_content is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=ErrorConstants.TABLE_OF_CONTENT_NOT_FOUND_MESSAGE
-            )
-
-        total_sections = await get_sections_count_of_table_of_content(
-            content_id=str(table_of_content.id)
-        )
-
-        detail_table_of_content = await SegmentUtils.get_mapped_segment_content_for_table_of_content(
-            table_of_content=table_of_content,
-            version_id=text_details_request.version_id
-        )   
-        detail_table_of_content = DetailTableOfContentResponse(
-            text_detail=text,
-            mapping=DetailTextMapping(
-                segment_id=text_details_request.segment_id,
-                section_id=text_details_request.section_id
-            ),
-            content=detail_table_of_content,
-            skip=text_details_request.skip,
-            current_section=min(total_sections,text_details_request.skip + 1),
-            limit=text_details_request.limit,
-            total=total_sections
-        )
-        new_cached_text_detail = await set_text_details_cache(
-            text_id=text_id,
-            content_id=str(table_of_content.id),
-            version_id=text_details_request.version_id,
-            skip=text_details_request.skip,
-            limit=text_details_request.limit,
-            text_details=detail_table_of_content
-        )
-        return detail_table_of_content
-            
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ErrorConstants.TEXT_NOT_FOUND_MESSAGE)
-
+    '''
+       In the following process first we will get the table of content if content_id is provided
+       Otherwise the frontend should have send segment_id which means we'll have to search this segment_id in table of contents
+       Later after getting the table of content we need to map each segment_id with it's content
+       If version ID is provied then we need to first identify the text_id in the mapping field and check if it's matching with the version_id, if yes we just map the segment_id with it's content
+       '''
+    await _validate_text_detail_request(text_id=text_id, text_details_request=text_details_request)
+    selected_text = await TextUtils.get_text_detail_by_id(text_id=text_id)
+    table_of_content = await _receive_table_of_content(
+        text_id=text_id,
+        text_details_request=text_details_request
+    )
+    # Check if the table of content exists in the cache database
+    cached_data = await get_text_details_cache(
+        text_id=text_id,
+        content_id=str(table_of_content.id),
+        version_id=text_details_request.version_id,
+        skip=text_details_request.skip,
+        limit=text_details_request.limit
+    )
+    # if the same table of content with same skip and limit is again found when searching segments in toc. It's returned from cache
+    if cached_data is not None:
+        return cached_data
+    detail_table_of_content = await _mapping_table_of_content(
+        text=selected_text,
+        table_of_content=table_of_content,
+        text_details_request=text_details_request
+    )
+    await set_text_details_cache(
+        text_id=text_id,
+        content_id=str(table_of_content.id),
+        version_id=text_details_request.version_id,
+        skip=text_details_request.skip,
+        limit=text_details_request.limit,
+        text_details=detail_table_of_content
+    )
+    return detail_table_of_content
 
 
 async def get_text_versions_by_group_id(text_id: str, language: str, skip: int, limit: int) -> TextVersionResponse:
@@ -263,7 +155,7 @@ async def get_text_versions_by_group_id(text_id: str, language: str, skip: int, 
         for table_of_content in list_of_table_of_contents:
             list_of_table_of_contents_ids.append(str(table_of_content.id))
         versions_table_of_content_id_dict[str(version.id)] = list_of_table_of_contents_ids
-    
+
     list_of_version = [
         TextVersion(
             id=str(version.id),
@@ -322,7 +214,8 @@ async def create_table_of_content(table_of_content_request: TableOfContent, toke
             segment_ids = TextUtils.get_all_segment_ids(table_of_content=table_of_content_request)
             valid_segments = await SegmentUtils.validate_segments_exists(segment_ids=segment_ids)
             if not valid_segments:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ErrorConstants.SEGMENT_NOT_FOUND_MESSAGE)
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                    detail=ErrorConstants.SEGMENT_NOT_FOUND_MESSAGE)
             table_of_content = await create_table_of_content_detail(table_of_content_request=table_of_content_request)
             return table_of_content
         else:
@@ -331,3 +224,121 @@ async def create_table_of_content(table_of_content_request: TableOfContent, toke
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=ErrorConstants.ADMIN_ERROR_MESSAGE)
 
 
+async def _mapping_table_of_content(text: Text, table_of_content: TableOfContent,
+                                    text_details_request: TextDetailsRequest):
+    total_sections = await get_sections_count_of_table_of_content(
+        content_id=str(table_of_content.id)
+    )
+    detail_table_of_content = await SegmentUtils.get_mapped_segment_content_for_table_of_content(
+        table_of_content=table_of_content,
+        version_id=text_details_request.version_id
+    )
+    detail_table_of_content = DetailTableOfContentResponse(
+        text_detail=text,
+        mapping=DetailTextMapping(
+            segment_id=text_details_request.segment_id,
+            section_id=text_details_request.section_id
+        ),
+        content=detail_table_of_content,
+        skip=text_details_request.skip,
+        current_section=min(total_sections, text_details_request.skip + 1),
+        limit=text_details_request.limit,
+        total=total_sections
+    )
+    return detail_table_of_content
+
+async def _receive_table_of_content(text_id: str, text_details_request: TextDetailsRequest):
+    table_of_content: TableOfContent = None
+    if text_details_request.content_id is not None:
+        table_of_content = await get_table_of_content_by_content_id(
+            content_id=text_details_request.content_id,
+            skip=text_details_request.skip,
+            limit=text_details_request.limit
+        )
+    elif text_details_request.segment_id is not None:
+        table_of_content = await TextUtils.get_table_of_content_id_and_respective_section_by_segment_id(
+            text_id=text_id,
+            segment_id=text_details_request.segment_id
+        )
+        text_details_request.skip = max(0, table_of_content.sections[0].section_number - 1)
+        text_details_request.limit = 1
+    if table_of_content is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=ErrorConstants.TABLE_OF_CONTENT_NOT_FOUND_MESSAGE
+        )
+    return table_of_content
+
+
+async def _validate_text_detail_request(text_id: str, text_details_request: TextDetailsRequest) -> bool:
+    # Check if text_id is provided
+    if text_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ErrorConstants.TEXT_OR_TERM_NOT_FOUND_MESSAGE
+        )
+
+    # Check if valid version_id is provided
+    if text_details_request.version_id is not None:
+        is_valid_version = await TextUtils.validate_text_exists(
+            text_id=text_details_request.version_id
+        )
+        if not is_valid_version:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=ErrorConstants.VERSION_NOT_FOUND_MESSAGE
+            )
+
+    # Check if valid segment_id is provided
+    if text_details_request.segment_id is not None:
+        is_valid_segment = await SegmentUtils.validate_segment_exists(
+            segment_id=text_details_request.segment_id
+        )
+        if not is_valid_segment:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=ErrorConstants.SEGMENT_NOT_FOUND_MESSAGE
+            )
+
+    is_valid_text = await TextUtils.validate_text_exists(text_id=text_id)
+    if not is_valid_text:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ErrorConstants.TEXT_NOT_FOUND_MESSAGE)
+
+
+async def _get_texts_by_term_id(term_id: str, language: str, skip: int, limit: int) -> List[Text]:
+    texts = await get_texts_by_term(term_id=term_id, language=language, skip=skip, limit=limit)
+    filter_text_base_on_group_id_type = await TextUtils.filter_text_base_on_group_id_type(texts=texts,
+                                                                                          language=language)
+    root_text = filter_text_base_on_group_id_type["root_text"]
+    commentary = filter_text_base_on_group_id_type["commentary"]
+    text_list = [
+        Text(
+            id=str(text.id),
+            title=text.title,
+            language=text.language,
+            type="commentary",
+            group_id=text.group_id,
+            is_published=text.is_published,
+            created_date=text.created_date,
+            updated_date=text.updated_date,
+            published_date=text.published_date,
+            published_by=text.published_by
+        )
+        for text in commentary
+    ]
+    if root_text is not None:
+        text_list.append(
+            Text(
+                id=str(root_text.id),
+                title=root_text.title,
+                language=root_text.language,
+                type="root_text",
+                group_id=root_text.group_id,
+                is_published=root_text.is_published,
+                created_date=root_text.created_date,
+                updated_date=root_text.updated_date,
+                published_date=root_text.published_date,
+                published_by=root_text.published_by
+            )
+        )
+    return text_list
