b3beccfbaa6b4f9bdbb7656a81da4a65188cfa13
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-05-21 15:49:07 +0530

    added pecha_text_image generator script and subprocess runner for now

--- pecha_api/share/share_service.py
+++ pecha_api/share/share_service.py
@@ -1,11 +1,13 @@
 from fastapi import HTTPException
 from pecha_api.error_contants import ErrorConstants
+import subprocess
 
 from ..texts.segments.segments_service import get_segment_details_by_id
 from ..texts.segments.segments_utils import SegmentUtils
 from starlette import status
 from starlette.responses import StreamingResponse
 from .share_utils import ShareUtils
+from ..texts.texts_utils import TextUtils
 
 async def generate_image(segment_id: str, language: str):
     if segment_id is not None and segment_id != "":
@@ -14,8 +16,16 @@ async def generate_image(segment_id: str, language: str):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ErrorConstants.SEGMENT_NOT_FOUND_MESSAGE)
         segment = await get_segment_details_by_id(segment_id=segment_id)
 
-        cleaned_content = ShareUtils.clean_html(segment.content)
-        image_bytes = ShareUtils.create_image_bytes(cleaned_content, language=language)
+        text_id = segment.text_id
+        text_detail = await TextUtils.get_text_detail_by_id(text_id=text_id)
+
+        segment_text = segment.content
+        reference_text = text_detail.title
+
+        language = text_detail.language
+
+
+        subprocess.run(["python3", "pecha_api/share/pecha_text_image.py"], check=True)
         
         return StreamingResponse(image_bytes, media_type="image/png")
     else:
