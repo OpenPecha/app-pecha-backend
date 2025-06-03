4ed15d5781f8df5727c10f2167b391bcc24bb39d
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-06-03 09:29:32 +0530

    added try and except method to avoid uuid conversion error pron

--- pecha_api/texts/groups/groups_service.py
+++ pecha_api/texts/groups/groups_service.py
@@ -4,6 +4,7 @@ from fastapi import HTTPException
 from starlette import status
 from uuid import UUID
 from typing import Dict, List
+import logging
 
 from .groups_repository import (
     create_group,
@@ -18,21 +19,32 @@ from .groups_response_models import (
 )
 
 async def validate_group_exists(group_id: str) -> bool:
-    uuid_group_id = UUID(group_id)
-    is_exists = await check_group_exists(group_id=uuid_group_id)
-    if not is_exists:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=ErrorConstants.GROUP_NOT_FOUND_MESSAGE
-        )
-    return is_exists
+    is_exists = False
+    try:
+        uuid_group_id = UUID(group_id)
+        is_exists = await check_group_exists(group_id=uuid_group_id)
+        if not is_exists:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=ErrorConstants.GROUP_NOT_FOUND_MESSAGE
+            )
+        return is_exists
+    except ValueError:
+        logging.error(f"Invalid group_id provided: {group_id}")
+        return False
     
 async def get_groups_by_list_of_ids(group_ids: List[str]) -> Dict[str, GroupDTO]:
     return await get_groups_by_ids(group_ids=group_ids)
 
 async def get_group_details(group_id: str) -> GroupDTO:
-    uuid_group_id = UUID(group_id)
-    return await get_group_by_id(group_id=uuid_group_id)
+    try:
+        uuid_group_id = UUID(group_id)
+        return await get_group_by_id(group_id=uuid_group_id)
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ErrorConstants.INVALID_GROUP_ID_MESSAGE
+        )
 
 async def create_new_group(
     create_group_request: CreateGroupRequest,
