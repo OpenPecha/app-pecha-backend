49907662edea9d663eaf2fd80493d9da42666f16
Tenzin  Tsering <tenzintsering@Tenzins-MacBook-Pro.local>
2025-06-05 15:08:37 +0530

    test case for image generation for segment content success

--- tests/share/test_share_service.py
+++ tests/share/test_share_service.py
@@ -8,6 +8,8 @@ from pecha_api.share.share_response_models import (
     ShortUrlResponse,
     ShareRequest
 )
+from pecha_api.texts.segments.segments_service import SegmentDTO
+from pecha_api.texts.texts_response_models import TextDTO
 
 @pytest.mark.asyncio
 async def test_generate_short_url_with_logo():
@@ -20,6 +22,47 @@ async def test_generate_short_url_with_logo():
     )
     with patch("pecha_api.share.share_service.get_short_url", new_callable=AsyncMock) as mock_short_url:
         mock_short_url.return_value = mock_short_url_response
+        
+        response = await generate_short_url(share_request=share_request)
+        
+        assert response is not None
+        assert isinstance(response, ShortUrlResponse)
+        assert response.shortUrl == "https://pecha.io/share/123"
+
+@pytest.mark.asyncio
+async def test_generate_short_url_for_segment_content_success():
+    share_request = ShareRequest(
+        url="https://pecha.io/share/123",
+        segment_id="id_1",
+        language="en",
+    )
+    mock_short_url_response = ShortUrlResponse(
+        shortUrl="https://pecha.io/share/123"
+    )
+    mock_segment_details = SegmentDTO(
+        id="id_1",
+        text_id="text_1",
+        content="content_1",
+        mapping=[]
+    )
+    mock_text_detail = TextDTO(
+        id="text_1",
+        title="title_1",
+        language="en",
+        type="version",
+        group_id="group_1",
+        is_published=True,
+        created_date="2021-01-01",
+        updated_date="2021-01-01",
+        published_date="2021-01-01",
+        published_by="user_1",
+        categories=[],
+        parent_id="parent_1",
+    )
+    with patch("pecha_api.share.share_service.get_short_url", new_callable=AsyncMock, return_value=mock_short_url_response),\
+        patch("pecha_api.share.share_service.SegmentUtils.validate_segment_exists", new_callable=AsyncMock, return_value=True),\
+        patch("pecha_api.share.share_service.get_segment_details_by_id", new_callable=AsyncMock, return_value=mock_segment_details),\
+        patch("pecha_api.share.share_service.TextUtils.get_text_detail_by_id", new_callable=AsyncMock, return_value=mock_text_detail):
         
         response = await generate_short_url(share_request=share_request)
         
