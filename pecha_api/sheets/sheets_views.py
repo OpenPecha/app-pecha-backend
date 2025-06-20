2adfaf9663282f0629db5a709069df119b5c4ef3
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-06-20 16:42:45 +0530

    code refactor in sheet.views

--- pecha_api/sheets/sheets_views.py
+++ pecha_api/sheets/sheets_views.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from fastapi import APIRouter, Query, Form
+from fastapi import APIRouter
 from fastapi.security import HTTPBearer
 from starlette import status
 
@@ -9,10 +9,8 @@ from fastapi.security import HTTPAuthorizationCredentials
 from typing import Annotated
 
 
-
-
-from .sheets_service import get_sheets, get_sheets_by_user_id, create_new_sheet, upload_sheet_image_request
-from .sheets_response_models import CreateSheetRequest, SheetIdRequest
+from .sheets_service import create_new_sheet, upload_sheet_image_request
+from .sheets_response_models import CreateSheetRequest
 
 oauth2_scheme = HTTPBearer()
 sheets_router = APIRouter(
@@ -20,25 +18,6 @@ sheets_router = APIRouter(
     tags=["Sheets"]
 )
 
-
-@sheets_router.get("/topic", status_code=status.HTTP_200_OK)
-def get_topics(language: str | None):
-    return get_sheets(topic_id='', language=language)
-
-#get sheet with user_id with pagination
-@sheets_router.get("", status_code=status.HTTP_200_OK)
-async def get_sheets_by_user_id(
-    user_id: Optional[str] = None,
-    language: Optional[str] = None,
-    skip: int = Query(default=0, ge=0, description="Number of records to skip"),
-    limit: int = Query(default=10, ge=1, le=100, description="Number of records to return")
-):
-    return await get_sheets_by_user_id(
-        user_id=user_id,
-        language=language,
-        skip=skip,
-        limit=limit
-    )
 
 @sheets_router.post("", status_code=status.HTTP_201_CREATED)
 async def create_sheet(
