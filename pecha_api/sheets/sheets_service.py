331b280e2b62bf81707a9fed8aaed812fdce0bdf
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-06-20 16:33:13 +0530

    refactor code and remove non-required code

--- pecha_api/sheets/sheets_service.py
+++ pecha_api/sheets/sheets_service.py
@@ -51,64 +51,6 @@ from pecha_api.texts.segments.segments_response_models import (
 )
 from pecha_api.texts.segments.segments_service import create_new_segment
 
-async def get_sheets(topic_id: str,language: str) -> SheetsResponse:
-    sheets = get_sheets_by_topic(topic_id=topic_id)
-    publisher = Publisher(
-        id=str(uuid.uuid4()),
-        name='Person Name',
-        profile_url="",
-        image_url=""
-    )
-    if language is None:
-        language = get("DEFAULT_LANGUAGE")
-    sheets_list = [
-        SheetModel(
-            id=str(sheet.id),
-            title=sheet.titles.get(language, ""),
-            summary=sheet.summaries.get(language, ""),
-            publisher=publisher
-        )
-        for sheet in sheets
-    ]
-    sheet_response = SheetsResponse(sheets=sheets_list)
-    return sheet_response
-
-# new service for get_sheet_by_id
-async def get_sheets_by_user_id(user_id: str, language: str, skip: int, limit: int) -> SheetsResponse:
-    if language is None:
-        language = get("DEFAULT_LANGUAGE")
-    publisher = None
-    with SessionLocal() as db_session:
-        publisher = get_user_by_username(db=db_session, username=user_id)
-        publisher = Publisher(
-            id=str(publisher.id),
-            name=publisher.firstname + " " + publisher.lastname,
-            profile_url="",
-            image_url=""
-        )
-    sheets = await get_users_sheets(user_id=user_id, language=language, skip=skip, limit=limit)
-    sheets_list = []
-    for sheet in sheets:
-        topic_list = []
-        for topic_id in sheet.topic_id:
-            topic = await get_topic_by_id(topic_id)
-            topic_list.append(topic.titles)
-        sheets_list.append(
-            SheetModel(
-                id=str(sheet.id),
-                title=sheet.titles,
-                summary=sheet.summaries,
-                publisher=publisher,
-                published_date=sheet.published_date,
-                time_passed=sheet.published_date,
-                views=sheet.views,
-                likes=sheet.likes,
-                topics=topic_list,
-                language=language
-            )
-        )
-    sheet_response = SheetsResponse(sheets=sheets_list)
-    return sheet_response
     
 async def create_new_sheet(create_sheet_request: CreateSheetRequest, token: str) -> TableOfContent:
     group_id =  await _create_sheet_group_(token=token)
@@ -129,6 +71,32 @@ async def create_new_sheet(create_sheet_request: CreateSheetRequest, token: str)
         token=token
     )
     return sheet_table_of_content
+
+
+
+def upload_sheet_image_request(sheet_id: Optional[str], file: UploadFile) -> SheetImageResponse:
+    # Validate and compress the uploaded image
+    image_utils = ImageUtils()
+    compressed_image = image_utils.validate_and_compress_image(file=file, content_type=file.content_type)
+    file_name, ext = os.path.splitext(file.filename)
+    unique_id = str(uuid.uuid4())
+    
+    # If no id is provided, use a random UUID as the folder name
+    path = f"images/sheet_images"
+    image_path_full = f"{path}/{sheet_id}/{unique_id}" if sheet_id is not None else f"{path}/{unique_id}"
+    sheet_image_name = f"{image_path_full}/{file_name}{ext}"
+    upload_key = upload_bytes(
+        bucket_name=get("AWS_BUCKET_NAME"),
+        s3_key=sheet_image_name,
+        file=compressed_image,
+        content_type=file.content_type
+    )
+    presigned_url = generate_presigned_upload_url(
+        bucket_name=get("AWS_BUCKET_NAME"),
+        s3_key=upload_key
+    )
+    
+    return SheetImageResponse(url=presigned_url)
 
 async def _generate_and_upload_sheet_table_of_content(
         create_sheet_request: CreateSheetRequest,
@@ -249,28 +217,3 @@ async def _create_sheet_group_(token: str) -> str:
     )
     new_group: GroupDTO = await create_new_group(create_group_request=create_group_request, token=token)
     return new_group.id
-
-
-def upload_sheet_image_request(sheet_id: Optional[str], file: UploadFile) -> SheetImageResponse:
-    # Validate and compress the uploaded image
-    image_utils = ImageUtils()
-    compressed_image = image_utils.validate_and_compress_image(file=file, content_type=file.content_type)
-    file_name, ext = os.path.splitext(file.filename)
-    unique_id = str(uuid.uuid4())
-    
-    # If no id is provided, use a random UUID as the folder name
-    path = f"images/sheet_images"
-    image_path_full = f"{path}/{sheet_id}/{unique_id}" if sheet_id is not None else f"{path}/{unique_id}"
-    sheet_image_name = f"{image_path_full}/{file_name}{ext}"
-    upload_key = upload_bytes(
-        bucket_name=get("AWS_BUCKET_NAME"),
-        s3_key=sheet_image_name,
-        file=compressed_image,
-        content_type=file.content_type
-    )
-    presigned_url = generate_presigned_upload_url(
-        bucket_name=get("AWS_BUCKET_NAME"),
-        s3_key=upload_key
-    )
-    
-    return SheetImageResponse(url=presigned_url)
