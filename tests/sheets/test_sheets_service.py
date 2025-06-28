c50321119b36a2d3aae183628e89456de24bbc7f
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-06-28 14:08:49 +0530

    refactor checking the condition for directly accesing index in a list

--- tests/sheets/test_sheets_service.py
+++ tests/sheets/test_sheets_service.py
@@ -11,7 +11,7 @@ from pecha_api.sheets.sheets_response_models import (
     CreateSheetRequest,
     Source,
     SheetIdResponse,
-    SheetDTO,
+    SheetDetailDTO,
     SheetSection
 )
 from pecha_api.texts.segments.segments_enum import SegmentType
@@ -27,7 +27,7 @@ from pecha_api.texts.texts_enums import TextType
 from pecha_api.sheets.sheets_service import (
     create_new_sheet,
     update_sheet_by_id,
-    get_sheet_by_id
+    get_sheet_by_id,
 )
 from pecha_api.users.users_models import Users
 from pecha_api.texts.segments.segments_response_models import (
@@ -345,12 +345,13 @@ async def test_get_sheet_by_id_success():
         response = await get_sheet_by_id(sheet_id=sheet_id, skip=0, limit=10)
 
         assert response is not None
-        assert isinstance(response, SheetDTO)
+        assert isinstance(response, SheetDetailDTO)
         assert response.id == sheet_id
         assert response.sheet_title == "sheet_title"
         assert response.publisher is not None
         assert isinstance(response.content, SheetSection)
         assert response.content is not None
+        
 
 @pytest.mark.asyncio
 async def test_get_sheet_by_id_invalid_sheet_id():
@@ -360,3 +361,6 @@ async def test_get_sheet_by_id_invalid_sheet_id():
             await get_sheet_by_id(sheet_id=sheet_id, skip=0, limit=10)
         assert exc_info.value.status_code == status.HTTP_404_NOT_FOUND
         assert exc_info.value.detail == ErrorConstants.TEXT_NOT_FOUND_MESSAGE
+
+
+
