596ab9a44aabb33532b713866615b5448bdd9391
Tenzin  Tsering <tenzintsering630@gmail.com>
2025-07-08 11:30:11 +0530

    fix indentation error after merge conflict resolve

--- pecha_api/search/search_cache_service.py
+++ pecha_api/search/search_cache_service.py
@@ -3,12 +3,13 @@ from pecha_api.cache.cache_repository import (
     get_cache_data,
     set_cache
 )
-
-def get_search_cache(query: str, search_type: SearchType, text_id: str = None, skip: int = 0, limit: int = 10):
-
 from .search_response_models import (
     SearchResponse
 )
+from .search_enums import SearchType
+
+def get_search_cache(query: str, search_type: SearchType, text_id: str = None, skip: int = 0, limit: int = 10):
+
     payload = [query, search_type, text_id, skip, limit]
     hashed_key: str = Utils.generate_hash_key(payload = payload)
     cache_data = get_cache_data(hash_key = hashed_key)
