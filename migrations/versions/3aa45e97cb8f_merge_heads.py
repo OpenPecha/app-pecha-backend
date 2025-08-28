1b709870e1226058bd306d07314df9ee5ca0a7e5
lobsam <sanduplobzang@gmail.com>
2025-08-28 17:04:34 +0530

    Remove obsolete migrations for authors, plan reviews, favorites, and plan items; add new migration for plan-related tables

--- migrations/versions/3aa45e97cb8f_merge_heads.py
+++ migrations/versions/3aa45e97cb8f_merge_heads.py
@@ -1,8 +1,8 @@
-"""empty message
+"""merge heads
 
-Revision ID: f6722f4e0490
+Revision ID: 3aa45e97cb8f
 Revises: 05dc8a1bf552, e50ed6d8c309
-Create Date: 2025-02-04 07:57:44.773686
+Create Date: 2025-08-28 16:47:42.482052
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'f6722f4e0490'
+revision: str = '3aa45e97cb8f'
 down_revision: Union[str, None] = ('05dc8a1bf552', 'e50ed6d8c309')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
