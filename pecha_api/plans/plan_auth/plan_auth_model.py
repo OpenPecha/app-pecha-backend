886155a427c319a430458cde5106f3eaf316049f
Lungsangg <lungsang@esukhia.org>
2025-08-29 23:46:05 +0530

    plan_login_endpoint_initial_update

--- pecha_api/plans/plan_auth/plan_auth_model.py
+++ pecha_api/plans/plan_auth/plan_auth_model.py
@@ -2,6 +2,37 @@ from typing import Optional
 from datetime import datetime
 from uuid import UUID
 from pydantic import BaseModel
+from sqlalchemy import Column, String, DateTime, Boolean, UUID
+from ..db.database import Base
+from _datetime import datetime
+from uuid import uuid4
+import _datetime
+from sqlalchemy.orm import relationship
+
+class Author(Base):
+    __tablename__ = "authors"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    auth0_id = Column(String(255), unique=True, index=True, nullable=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    firstname = Column(String(255), nullable=False)
+    lastname = Column(String(255), nullable=True)
+    username = Column(String(255), nullable=True)
+    password = Column(String(255), nullable=True)
+    registration_source = Column(String(25), nullable=False)
+    title = Column(String(255),nullable=True)
+    organization = Column(String(255),nullable=True)
+    location = Column(String(255),nullable=True)
+    education = Column(String(255),nullable=True)
+    about_me = Column(String,nullable=True)
+    avatar_url = Column(String(255),nullable=True)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, server_default="FALSE", default=False,nullable=False)
+    created_at = Column(DateTime, default=datetime.now(_datetime.timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(_datetime.timezone.utc))
+
+    # Define the relationship between users and social media accounts
+    social_media_accounts = relationship("SocialMediaAccount", back_populates="author", cascade="all, delete-orphan")
+
 
 
 class CreateAuthorRequest(BaseModel):
@@ -20,3 +51,20 @@ class AuthorDetails(BaseModel):
 
 class AuthorResponse(BaseModel):
     author: AuthorDetails
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+class AuthorInfo(BaseModel):
+    name: str
+    avatar_url: Optional[str] = None
+
+class AuthorLoginRequest(BaseModel):
+    email: str
+    password: str
+
+class AuthorLoginResponse(BaseModel):
+    author: AuthorDetails
+    auth: TokenResponse
