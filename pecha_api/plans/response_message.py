4b227b790b4cadf0e388f03fe0bb1d80b7c28532
Lungsangg <lungsang@esukhia.org>
2025-09-02 15:22:15 +0530

    response_

--- pecha_api/plans/response_message.py
+++ pecha_api/plans/response_message.py
@@ -3,6 +3,8 @@ PASSWORD_LENGTH_INVALID = "Password must be between 8 and 20 characters"
 
 AUTHOR_NOT_FOUND = "Author not found"
 AUTHOR_UPDATE_INVALID = "Invalid update request"
+AUTHOR_NOT_VERIFIED = "Author not verified"
+AUTHOR_NOT_ACTIVE = "Author not active"
 
 TOKEN_TYPE_INVALID = "Invalid token type"
 TOKEN_PAYLOAD_INVALID = "Invalid token payload"
