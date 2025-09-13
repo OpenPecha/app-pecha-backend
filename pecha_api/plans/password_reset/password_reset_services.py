cdfa9397a51e9b59d53ee4b0bbee06e12d834e77
Lungsangg <lungsang@esukhia.org>
2025-09-13 14:07:13 +0530

    update_password_for_services

--- pecha_api/plans/password_reset/password_reset_services.py
+++ pecha_api/plans/password_reset/password_reset_services.py
@@ -33,3 +33,25 @@ def request_reset_password(email: str):
         reset_link = f"{get('BASE_URL')}/reset-password?token={reset_token}"
         send_reset_email(email=email, reset_link=reset_link)
         return {"message": "If the email exists in our system, a password reset email has been sent."}
+
+
+def update_password(token: str, password: str):
+    with SessionLocal() as db_session:
+        reset_entry = get_password_reset_by_token(
+            db=db_session,
+            token=token
+        )
+        if reset_entry is None or reset_entry.token_expiry < datetime.now(timezone.utc):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired token")
+        current_user = get_user_by_email(
+            db=db_session,
+            email=reset_entry.email
+        )
+        if current_user.registration_source != 'email':
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Registration Source Mismatch")
+
+        _validate_password(password)
+        hashed_password = get_hashed_password(password)
+        current_user.password = hashed_password
+        updated_user = save_user(db=db_session, user=current_user)
+        return updated_user
