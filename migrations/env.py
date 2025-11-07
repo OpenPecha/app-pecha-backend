bb459d4d9adfccc2eaf1dfc9a9b72ddd5a592680
TenzDelek <tibetdelek@gmail.com>
2025-11-07 10:13:43 +0530

    naming

--- migrations/env.py
+++ migrations/env.py
@@ -15,8 +15,8 @@ from pecha_api.plans.favorites.favorites_models import Favorite
 from pecha_api.plans.users.plan_users_models import UserPlanProgress
 from pecha_api.plans.users.plan_users_models import UserTaskCompletion
 from pecha_api.users.users_models import Users, SocialMediaAccount, PasswordReset
-from pecha_api.plans.recitation.recitation_models import Recitation
-from pecha_api.plans.users.recitation.user_recitation_model import UserRecitation
+from pecha_api.plans.recitation.plan_recitation_models import Recitation
+from pecha_api.plans.users.recitation.plan_user_recitation_model import UserRecitation
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
