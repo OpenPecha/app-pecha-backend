8e7d52cf13718ccb20f32faf45005c7ffe8bb41e
Lungsangg <lungsang@esukhia.org>
2025-11-10 16:52:15 +0530

    feat: implement featured day API endpoint with mock data

---
+++ pecha_api/plans/featured/featured_day_service.py
@@ -0,0 +1,8 @@
+
+MOCK_DATA = {
+    "GREETING" : "TASHI DELEK"
+}
+
+
+def get_featured_day_service():
+    return MOCK_DATA
--- pecha_api/plans/featured/featured_day_views.py
+++ pecha_api/plans/featured/featured_day_views.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from starlette import status
 
-
+from .featured_day_service import get_featured_day_service
 
 
 oauth2_scheme = HTTPBearer()
@@ -16,7 +16,5 @@ user_follow_router = APIRouter(
 
 @user_follow_router.get("/featured-day", status_code=status.HTTP_204_NO_CONTENT)
 def get_featured_day():
-    return {'status': 'up'}
-
-
+    return get_featured_day_service()
 
