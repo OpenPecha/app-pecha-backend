993d22e661e74d01fc0ce4c067bbc170cb073c0c
TenzDelek <tibetdelek@gmail.com>
2025-11-10 15:11:23 +0530

    changes

--- pecha_api/recitations/recitations_response_models.py
+++ pecha_api/recitations/recitations_response_models.py
@@ -1,14 +1,18 @@
 from typing import List
 from pydantic import BaseModel
 from uuid import UUID
-class Content(BaseModel):
+
+class TextSegments(BaseModel):
     text: str
     segment_id: str
-    start_time: float
-    end_time: float
+    start_time: str
+    end_time: str
+
+class RecitationContent(BaseModel):
+    content: List[TextSegments]
 
 class CreateRecitationsRequest(BaseModel):
     title: str
     audio_url: str
     text_id: UUID
-    content: dict
+    content: RecitationContent
