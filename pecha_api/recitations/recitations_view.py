1b981aa797e75dc54d265ca3806deb0ea1177541
TenzDelek <tibetdelek@gmail.com>
2025-11-11 15:14:54 +0530

    remove auth

--- pecha_api/recitations/recitations_view.py
+++ pecha_api/recitations/recitations_view.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 from starlette import status
-from fastapi.security import HTTPBearer
 
 from pecha_api.recitations.recitations_response_models import RecitationsResponse
 from pecha_api.recitations.recitations_services import get_list_of_recitations_service
 
-oauth2_scheme=HTTPBearer()
 recitation_router=APIRouter(
     prefix="/cms/recitations",
     tags=["Recitations"],
