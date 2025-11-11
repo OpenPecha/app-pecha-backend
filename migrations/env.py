e9d990ae6deeddcf718b611d041bf87ecadac5b5
TenzDelek <tibetdelek@gmail.com>
2025-11-11 16:22:33 +0530

    create user recitations

--- migrations/env.py
+++ migrations/env.py
@@ -15,6 +15,7 @@ from pecha_api.plans.favorites.favorites_models import Favorite
 from pecha_api.plans.users.plan_users_models import UserPlanProgress
 from pecha_api.plans.users.plan_users_models import UserTaskCompletion
 from pecha_api.users.users_models import Users, SocialMediaAccount, PasswordReset
+from pecha_api.plans.users.recitation.user_recitations_models import UserRecitations
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
