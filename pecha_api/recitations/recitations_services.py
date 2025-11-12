efa08b1b4f37b47b05e84053569e85a64f63245c
TenzDelek <tibetdelek@gmail.com>
2025-11-12 15:05:48 +0530

    added language query

--- pecha_api/recitations/recitations_services.py
+++ pecha_api/recitations/recitations_services.py
@@ -2,7 +2,7 @@ from pecha_api.texts.texts_models import Text
 from pecha_api.texts.texts_enums import TextType
 from pecha_api.recitations.recitations_response_models import RecitationDTO, RecitationsResponse
 
-async def get_list_of_recitations_service() -> RecitationsResponse:
-    recitations = await Text.get_list_of_recitations(type=TextType.RECITATION)
+async def get_list_of_recitations_service(language: str) -> RecitationsResponse:
+    recitations = await Text.get_list_of_recitations(type=TextType.RECITATION, language=language)
     recitations_dto = [RecitationDTO(title=recitation.title, text_id=recitation.id) for recitation in recitations]
     return RecitationsResponse(recitations=recitations_dto)
