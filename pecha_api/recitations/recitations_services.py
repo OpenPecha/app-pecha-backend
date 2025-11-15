81e2ce32b76688a9611bbcaa51b70304682f3054
TenzDelek <tibetdelek@gmail.com>
2025-11-15 23:10:21 +0530

    naming

--- pecha_api/recitations/recitations_services.py
+++ pecha_api/recitations/recitations_services.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pecha_api.collections.collections_repository import get_all_collections_by_parent, get_collection_id_by_slug
-from pecha_api.recitations.recitations_repository import apply_search_filter
+from pecha_api.recitations.recitations_repository import apply_search_recitation_title_filter
 from pecha_api.recitations.recitations_response_models import RecitationDTO, RecitationsResponse
 from pecha_api.texts.texts_service import get_root_text_by_collection_id
 from fastapi import HTTPException
@@ -20,6 +20,6 @@ async def get_list_of_recitations_service(search: Optional[str] = None, language
         if text_id is not None and text_title is not None:
             list_of_texts.append((text_id, text_title))
     if search:
-        list_of_texts=apply_search_filter(text=list_of_texts, search=search)
+        list_of_texts=apply_search_recitation_title_filter(text=list_of_texts, search=search)
     recitations_dto = [RecitationDTO(title=text_title, text_id=text_id) for text_id, text_title in list_of_texts]
     return RecitationsResponse(recitations=recitations_dto)
