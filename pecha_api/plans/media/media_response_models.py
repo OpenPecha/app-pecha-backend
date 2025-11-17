d36eca613014b1e534edfb4d745e2fde118720c5
tenzinsamten <iamsamten>
2025-11-17 21:45:38 +0100

    chore: saving image in different sizes while uploading

--- pecha_api/plans/media/media_response_models.py
+++ pecha_api/plans/media/media_response_models.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 from pecha_api.plans.response_message import IMAGE_UPLOAD_SUCCESS
 
+class ImageUrlModel(BaseModel):
+    thumbnail: str
+    medium: str
+    original: str
+
 class PlanUploadResponse(BaseModel):
-    url: str
+    image: ImageUrlModel
     key: str
     path: str
     message: str = IMAGE_UPLOAD_SUCCESS
