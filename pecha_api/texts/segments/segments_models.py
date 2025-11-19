5806c8acb6544a13e270ab2f3adc012fdf9d13f8
TenzDelek <122612557+TenzDelek@users.noreply.github.com>
2025-11-19 12:53:56 +0530

    Pecha id Mapping (#343)
    * get by pecha id
    
    * repository
    
    * mapping
    
    * semgnet
    
    * mapping logix
    
    * send id
    
    * update mapping test
    
    * unrelated test fix
    
    * remove unrealted
    
    * remove uinrealted comment
    
    * changes
    
    * test
    
    * uuid to string
    
    * new appraoch
    
    * test
    
    * fucnction

--- pecha_api/texts/segments/segments_models.py
+++ pecha_api/texts/segments/segments_models.py
@@ -25,9 +25,15 @@ class Segment(Document):
         ]
 
     @classmethod
+    async def get_segments_by_pecha_segment_ids(cls, pecha_segment_ids: List[str]) -> List["Segment"]:
+        pecha_segment_ids = [str(pecha_segment_id) for pecha_segment_id in pecha_segment_ids]
+        return await cls.find({cls.pecha_segment_id: {"$in": pecha_segment_ids}}).to_list()
+
+    @classmethod
     async def get_segment_by_id(cls, segment_id: str) -> Optional["Segment"]:
         return await cls.find_one(cls.id == uuid.UUID(segment_id))
 
+    
     @classmethod
     async def get_segments_by_ids(cls, segment_ids: List[str]) -> List["Segment"]:
         segment_ids = [uuid.UUID(segment_id) for segment_id in segment_ids]
